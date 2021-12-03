4c742e7cfdf44d63a46652f1760c4a0b3fdd682a
Alex Olson <alexkellenolson@gmail.com>
2021-12-04 00:50:28 +0100

    wip day 1 part 2

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::fs;
+use std::{fs, vec};
 
 fn day1() {
     let input = fs::read_to_string("input/day1.txt").unwrap();
@@ -18,6 +18,21 @@ fn day1() {
     }
 
     println!("day 1 part 1 answer: {:#}", increases);
+
+    let window_sums: Vec<u32> = Vec::new();
+    let mut window_increases: u32 = 0;
+
+    for (i, m) in measurements.iter().enumerate() {
+        if i == 0 {
+            continue;
+        }
+
+        let prev = measurements.get(i - 1);
+
+        if m.trim().parse::<u32>().unwrap() > prev.unwrap().trim().parse::<u32>().unwrap() {
+            increases += 1;
+        }
+    }
 }
 
 fn main() {
