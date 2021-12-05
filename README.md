a012636baa92dc29e13548273a1ca5c5a57dbd22
Alex Olson <alexkellenolson@gmail.com>
2021-12-05 23:54:50 +0100

    finish day 1

--- README.md
+++ README.md
@@ -6,9 +6,9 @@ To try out the solutions clone the repo and run `cargo run` from the repository 
 
 ## Status
 
-- [ ] Day 1
+- [x] Day 1
     - [x] Part 1
-    - [ ] Part 2
+    - [x] Part 2
 - [ ] Day 2
     - [ ] Part 1
     - [ ] Part 2
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::{fs, vec};
+use std::fs;
 
 fn day1() {
     let input = fs::read_to_string("input/day1.txt").unwrap();
@@ -17,22 +17,45 @@ fn day1() {
         }
     }
 
-    println!("day 1 part 1 answer: {:#}", increases);
+    println!("day 1 part 1 answer: {}", increases);
 
-    let window_sums: Vec<u32> = Vec::new();
-    let mut window_increases: u32 = 0;
+    const WINDOW_SIZE: usize = 3;
 
-    for (i, m) in measurements.iter().enumerate() {
+    let mut prev_window_sum;
+    let mut current_window_sum = 0;
+    let mut win_increases: u32 = 0;
+
+    let mut k = 0;
+
+    while k < WINDOW_SIZE {
+        current_window_sum += measurements.get(k).unwrap().parse::<u32>().unwrap();
+        k += 1;
+    }
+
+    for (i, _m) in measurements.iter().enumerate() {
         if i == 0 {
             continue;
         }
 
-        let prev = measurements.get(i - 1);
+        if let None = measurements.get(i + WINDOW_SIZE - 1) {
+            break;
+        }
 
-        if m.trim().parse::<u32>().unwrap() > prev.unwrap().trim().parse::<u32>().unwrap() {
-            increases += 1;
+        prev_window_sum = current_window_sum;
+
+        current_window_sum -= measurements.get(i - 1).unwrap().parse::<u32>().unwrap();
+        current_window_sum += measurements
+            .get(i + WINDOW_SIZE - 1)
+            .unwrap()
+            .parse::<u32>()
+            .unwrap();
+
+        if current_window_sum > prev_window_sum {
+            win_increases += 1;
         }
     }
+
+    println!("day 1 part 2 answer: {}", win_increases);
 }
 
 fn main() {
