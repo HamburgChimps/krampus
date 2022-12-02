573c328d74dbf2579011cb3afa060e7109293c9f
Alex Olson <alexkellenolson@gmail.com>
2022-12-02 22:24:12 +0100

    broken

--- 2022/src/main/java/de/hamburgchimps/krampus/twentytwentytwo/Solution.java
+++ 2022/src/main/java/de/hamburgchimps/krampus/twentytwentytwo/Solution.java
@@ -1,10 +1,13 @@
 package de.hamburgchimps.krampus.twentytwentytwo;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
 import java.util.function.Supplier;
 
 public final class Solution {
-
     private static final List<List<Supplier<Result>>> store = List.of(DayOne.store);
 
     public static Result execute(int day, int part) {
@@ -14,17 +17,27 @@ public final class Solution {
                 .get();
     }
     public static final class DayOne {
-        // TODO: @next read input data
+        private static List<Integer> input = getInput("day1.txt");
         private static final List<Supplier<Result>> store = List.of(DayOne::PartOne, DayOne::PartTwo);
 
         private static Result PartOne() {
-            return new Result(true);
+            return new Result(input, true);
         }
 
         private static Result PartTwo() {
-            return new Result(true);
+            return new Result(input, true);
         }
     }
 
-    public record Result(boolean success) {}
+    public record Result(List<Integer> input, boolean success) {}
+
+    private static List<Integer> getInput(String path) {
+        try (var lines = Files.lines(Path.of(Solution.class.getClassLoader().getResource("input" + "/day1.txt").getPath()))) {
+            return lines
+                    .map(Integer::parseInt)
+                    .toList();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
