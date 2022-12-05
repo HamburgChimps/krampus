b728cbbbdec54646a1e0a117422d88c14494f232
Alex Olson <alexkellenolson@gmail.com>
2022-12-05 23:28:11 +0100

    get input in imperative way

--- 2022/src/main/java/de/hamburgchimps/krampus/twentytwentytwo/Solution.java
+++ 2022/src/main/java/de/hamburgchimps/krampus/twentytwentytwo/Solution.java
@@ -3,6 +3,7 @@ package de.hamburgchimps.krampus.twentytwentytwo;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Supplier;
 
@@ -17,33 +18,48 @@ public final class Solution {
     }
 
     public static final class DayOne {
-        private static final List<Integer> input = getInput("day1.txt");
+        private static final List<String> input = getInput("day1.txt");
         private static final List<Supplier<Result>> store = List.of(DayOne::PartOne, DayOne::PartTwo);
 
         private static Result PartOne() {
-            return new Result(input, true);
+            var inventories = new ArrayList<List<Integer>>();
+            var currentInventoryIdx = 0;
+
+            inventories.add(currentInventoryIdx, new ArrayList<>());
+
+            for (String line: input) {
+                if (line.isEmpty()) {
+                    inventories.add(++currentInventoryIdx, new ArrayList<>());
+                    continue;
+                }
+
+                inventories
+                        .get(currentInventoryIdx)
+                        .add(Integer.parseInt(line));
+            }
+
+            return new Result(input, inventories, true);
         }
 
         private static Result PartTwo() {
-            return new Result(input, true);
+            return new Result(input, List.of(),true);
         }
     }
 
-    public record Result(List<Integer> input, boolean success) {
+    public record Result(List<String> input, List<List<Integer>> output, boolean success) {
     }
 
     // Would not have figured this out without https://stackoverflow.com/a/46613809/205930
     // Who knew reading files in the resource folder was so complicated?
-    private static List<Integer> getInput(String path) {
+    private static List<String> getInput(String path) {
         try (var stream = Solution.class.getClassLoader().getResourceAsStream("input" + "/" + path)) {
             if (stream == null) {
                 return List.of();
             }
             var reader = new InputStreamReader(stream);
             var br = new BufferedReader(reader);
+
             return br.lines()
-                    .filter((l) -> !l.isBlank())
-                    .map(Integer::parseInt)
                     .toList();
         } catch (IOException e) {
             throw new RuntimeException(e);
